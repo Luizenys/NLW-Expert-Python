16e00f9be1a4ae541f0cc2bad079797672d402db
Luiza Rodrigues <luizars@id.uff.br>
2024-02-07 21:44:37 -0300

    implementing view for tag creator with http types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,12 @@
+from src.views.htttp_types.http_request import HttpRequest
+from src.views.htttp_types.http_response import HttpResponse
+
+class TagCreatorView:
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        product_code = body["product_code"]
+
+        print('Estou na minha view')
+        print(product_code)
+
+        return HttpResponse(status_code=200, body={"hello": "world"})
